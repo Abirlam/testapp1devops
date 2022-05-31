3c7d40ba957da43bb40df98c6b574fec1348f285
Abirlam <104069118+Abirlam@users.noreply.github.com>
2022-05-31 14:55:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_pytest_example.handlers.routes import configure_routes
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 configure_routes(app)
 
 if __name__ == '__main__':
